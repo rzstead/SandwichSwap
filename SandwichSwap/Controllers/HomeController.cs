dd7c2a91f6663655ea896451b959d1d779c26e12
rzstead <rzstea@gmail.com>
2016-11-01 18:12:02 -0600

    Added view frames

---
+++ SandwichSwap/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SandwichSwap.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult UserProf()
+        {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            return View();
+        }
+        public ActionResult Order()
+        {
+            return View();
+        }
+    }
+}
