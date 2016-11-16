5be0e68548a256dd80231a142138dc083a1b68e4
timafer <boomer3000@comcast.net>
2016-11-15 17:15:24 -0700

    finaly added this

---
+++ SandwichSwap/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SandwichSwap
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+    }
+}
